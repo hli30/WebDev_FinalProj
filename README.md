7c3ffdfdb9858da49c35b6a6310c80627e6291d3
hli30 <harrylll93@gmail.com>
2018-04-04 06:22:11 +0000

    set up react and related dependencies

---
+++ README.md
@@ -0,0 +1,4 @@
+## Base Setup
+
+run `npm install` then `npm build`
+once its built, only need `npm start` from now on to start sever, runs on port 3000
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: __dirname + '/app/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.sass$/,
+        loaders: ["style-loader", "css-loder", "sass-loader"]
+      }
+    ]
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  },
+  plugins: [HTMLWebpackPluginConfig],
+  mode: 'development'
+};
